6821fe72e68742eab7447c76898fd454d82733fb
Bordiuzhan <bordyujan@gmail.com>
2023-02-19 15:48:05 +0100

    refactored Joi validation

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,18 +1,13 @@
 const contacts = require('../models/index');
-const { HttpError } = require('../helpers/index');
 const { ctrlWrapper } = require('../helpers/index');
-const Joi = require('joi');
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-});
+const { HttpError } = require('../helpers');
 
-const getAll = async (req, res, next) => {
+const getAll = async (req, res) => {
   const result = await contacts.listContacts();
   res.json(result);
 };
 
-const getBuId = async (req, res, next) => {
+const getBuId = async (req, res) => {
   const { contactId } = req.params;
   const result = await contacts.getContactById(contactId);
   console.log(result);
@@ -22,20 +17,12 @@ const getBuId = async (req, res, next) => {
   res.json(result);
 };
 
-const add = async (req, res, next) => {
-  const { error } = addSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, error.message);
-  }
+const add = async (req, res) => {
   const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
 
-const updateBuId = async (req, res, next) => {
-  const { error } = addSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, error.message);
-  }
+const updateBuId = async (req, res) => {
   const { contactId } = req.params;
   const result = await contacts.updateBuId(contactId, req.body);
   if (!result) {
@@ -44,7 +31,7 @@ const updateBuId = async (req, res, next) => {
   res.json(result);
 };
 
-const deleteBuId = async (req, res, next) => {
+const deleteBuId = async (req, res) => {
   const { contactId } = req.params;
   const result = await contacts.removeContact(contactId);
   if (!result) {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../../controllers/contacts');
+const { validateBody } = require('../../madelwares');
+const schema = require('../../shemas/contacts');
 
 router.get('/', ctrl.getAll);
 
 router.get('/:contactId', ctrl.getBuId);
 
-router.post('/', ctrl.add);
+router.post('/', validateBody(schema.addSchema), ctrl.add);
 
-router.put('/:contactId', ctrl.updateBuId);
+router.put('/:contactId', validateBody(schema.addSchema), ctrl.updateBuId);
 
 router.delete('/:contactId', ctrl.deleteBuId);
 module.exports = router;

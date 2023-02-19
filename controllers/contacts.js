d726d2c430110270641ef27d2ccbe99e75031e1b
Bordiuzhan <bordyujan@gmail.com>
2023-02-19 13:16:39 +0100

    added decorator

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,6 @@
-const contacts = require('../../models/index');
-const { HttpError } = require('../../helpers/index');
+const contacts = require('../models/index');
+const { HttpError } = require('../helpers/index');
+const { ctrlWrapper } = require('../helpers/index');
 const Joi = require('joi');
 const addSchema = Joi.object({
   name: Joi.string().required(),
@@ -7,77 +8,58 @@ const addSchema = Joi.object({
 });
 
 const getAll = async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+  const result = await contacts.listContacts();
+  res.json(result);
 };
 
 const getBuId = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
-    console.log(result);
-    if (!result) {
-      throw HttpError(404, 'Server not found');
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  console.log(result);
+  if (!result) {
+    throw HttpError(404, 'Server not found');
   }
+  res.json(result);
 };
 
 const add = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
 };
 
 const updateBuId = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { contactId } = req.params;
-    const result = await contacts.updateBuId(contactId, req.body);
-    if (!result) {
-      throw HttpError(404, 'Server not found');
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const { contactId } = req.params;
+  const result = await contacts.updateBuId(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, 'Server not found');
+  }
+  res.json(result);
 };
 
 const deleteBuId = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw HttpError(404, 'Server not found');
-    }
-    res.json({
-      message: 'Delete success',
-    });
-    // res.status(204).send() // 204 Не передає меседж
-  } catch (error) {
-    next(error);
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, 'Server not found');
   }
+  res.json({
+    message: 'Delete success',
+  });
+  // res.status(204).send() // 204 Не передає меседж
 };
+
 module.exports = {
-  getAll,
-  getBuId,
-  add,
-  updateBuId,
-  deleteBuId,
+  getAll: ctrlWrapper(getAll),
+  getBuId: ctrlWrapper(getBuId),
+  add: ctrlWrapper(add),
+  updateBuId: ctrlWrapper(updateBuId),
+  deleteBuId: ctrlWrapper(deleteBuId),
 };

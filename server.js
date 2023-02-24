23821ab788238dc2ab5a3462d00adc6387332c1d
Bordiuzhan <bordyujan@gmail.com>
2023-02-24 12:41:21 +0100

    task 1 task 2 task 3 task 4

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+const app = require('./app');
+const DB_HOST =
+  'mongodb+srv://oleks:freedom2023@cluster0.cgf1jdk.mongodb.net/contacts_reader?retryWrites=true&w=majority';
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log('Database connection successful');
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });

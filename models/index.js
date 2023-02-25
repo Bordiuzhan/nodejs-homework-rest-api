24127b5256e2ebdea5a05174501b96c083b34f06
Bordiuzhan <bordyujan@gmail.com>
2023-02-25 20:52:19 +0100

    add  find

--- models/index.js
+++ models/index.js
@@ -1,77 +1,77 @@
-const fs = require('fs/promises');
-const path = require('path');
+// const fs = require('fs/promises');
+// const path = require('path');
 
-const contactsPath = path.join(__dirname, '/contacts.json');
+// const contactsPath = path.join(__dirname, '/contacts.json');
 
-const genId = async () => {
-  try {
-    const contacts = await listContacts();
-    const contactsId = contacts.map((el) => parseInt(el.id));
-    const id = Math.max(...contactsId) + 1;
-    return id.toString();
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-const rewriteContacts = async (data) => {
-  try {
-    const newContacts = JSON.stringify(data, null, '\t');
-    await fs.writeFile(contactsPath, newContacts);
-    return;
-  } catch (error) {
-    console.log(error.messsage);
-  }
-};
+// const genId = async () => {
+//   try {
+//     const contacts = await listContacts();
+//     const contactsId = contacts.map((el) => parseInt(el.id));
+//     const id = Math.max(...contactsId) + 1;
+//     return id.toString();
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+// const rewriteContacts = async (data) => {
+//   try {
+//     const newContacts = JSON.stringify(data, null, '\t');
+//     await fs.writeFile(contactsPath, newContacts);
+//     return;
+//   } catch (error) {
+//     console.log(error.messsage);
+//   }
+// };
 
-const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(data);
-  return contacts;
-};
+// const listContacts = async () => {
+//   const data = await fs.readFile(contactsPath);
+//   const contacts = JSON.parse(data);
+//   return contacts;
+// };
 
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  const foundedContact = contacts.find(
-    (el) => parseInt(el.id) === Number(contactId)
-  );
-  return foundedContact || null;
-};
+// const getContactById = async (contactId) => {
+//   const contacts = await listContacts();
+//   const foundedContact = contacts.find(
+//     (el) => parseInt(el.id) === Number(contactId)
+//   );
+//   return foundedContact || null;
+// };
 
-const updateById = async (id, data) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((el) => parseInt(el.id) === Number(id));
-  if (index === -1) {
-    return null;
-  }
-  contacts[index] = { ...contacts[index], ...data };
-  await rewriteContacts(contacts);
-  return contacts[index];
-};
+// const updateById = async (id, data) => {
+//   const contacts = await listContacts();
+//   const index = contacts.findIndex((el) => parseInt(el.id) === Number(id));
+//   if (index === -1) {
+//     return null;
+//   }
+//   contacts[index] = { ...contacts[index], ...data };
+//   await rewriteContacts(contacts);
+//   return contacts[index];
+// };
 
-const removeContact = async (id) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((el) => parseInt(el.id) === Number(id));
-  if (index === -1) {
-    return null;
-  }
-  const [result] = contacts.splice(index, 1);
-  await rewriteContacts(contacts);
-  console.log(`The contact with ID: ${id} was removed! Contact list: `);
-  return result;
-};
+// const removeContact = async (id) => {
+//   const contacts = await listContacts();
+//   const index = contacts.findIndex((el) => parseInt(el.id) === Number(id));
+//   if (index === -1) {
+//     return null;
+//   }
+//   const [result] = contacts.splice(index, 1);
+//   await rewriteContacts(contacts);
+//   console.log(`The contact with ID: ${id} was removed! Contact list: `);
+//   return result;
+// };
 
-const addContact = async (name, email, phone) => {
-  const contacts = await listContacts();
-  const newContact = { id: await genId(), name, email, phone };
-  contacts.push(newContact);
-  await rewriteContacts(contacts);
-  return newContact;
-};
+// const addContact = async (name, email, phone) => {
+//   const contacts = await listContacts();
+//   const newContact = { id: await genId(), name, email, phone };
+//   contacts.push(newContact);
+//   await rewriteContacts(contacts);
+//   return newContact;
+// };
 
-module.exports = {
-  listContacts,
-  getContactById,
-  updateById: updateById,
-  removeContact,
-  addContact,
-};
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   updateById: updateById,
+//   removeContact,
+//   addContact,
+// };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../../controllers/contacts');
-const { validateBody } = require('../../madelwares');
-const schema = require('../../shemas/contacts');
+// const { validateBody } = require('../../madelwares');
+// const schema = require('../../shemas/contacts');
 
 router.get('/', ctrl.getAll);
 
-router.get('/:contactId', ctrl.getById);
+// router.get('/:contactId', ctrl.getById);
 
-router.post('/', validateBody(schema.addSchema), ctrl.add);
+// router.post('/', validateBody(schema.addSchema), ctrl.add);
 
-router.put('/:contactId', validateBody(schema.updateSchema), ctrl.updateById);
+// router.put('/:contactId', validateBody(schema.updateSchema), ctrl.updateById);
 
-router.delete('/:contactId', ctrl.deleteById);
+// router.delete('/:contactId', ctrl.deleteById);
 module.exports = router;

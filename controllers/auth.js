2fd88e89cec909ab03a51fb50a2c8ac98887b36f
Bordiuzhan <bordyujan@gmail.com>
2023-03-10 00:14:46 +0100

    add to schema avatarURL add creation avatar default

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+
 const { User } = require('../models/user');
 const { HttpError, ctrlWrapper } = require('../helpers');
 const { SECRET_KEY } = process.env;
@@ -11,8 +13,13 @@ const register = async (req, res) => {
     throw HttpError(409, 'Email in use');
   }
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     user: {
